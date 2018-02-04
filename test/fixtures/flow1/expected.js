3718ef59b0b2fcb506248d34f45ece35c537b9fe
Kevin Barabash <kevinb@khanacademy.org>
2018-02-04 18:14:31 -0500

    upgrade react-docgen to v3-beta10

---
+++ test/fixtures/flow1/expected.js
@@ -0,0 +1,68 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Button = function Button(_ref) {
+  var children = _ref.children,
+      onClick = _ref.onClick,
+      style = _ref.style;
+  return _react2.default.createElement(
+    'button',
+    {
+      style: {},
+      onClick: onClick
+    },
+    children
+  );
+};
+exports.default = Button;
+Button.__docgenInfo = {
+  'description': '',
+  'props': {
+    'children': {
+      'flowType': {
+        'name': 'string'
+      },
+      'required': true,
+      'description': ''
+    },
+    'onClick': {
+      'flowType': {
+        'name': 'signature',
+        'type': 'function',
+        'raw': '() => any',
+        'signature': {
+          'arguments': [],
+          'return': {
+            'name': 'any'
+          }
+        }
+      },
+      'required': false,
+      'description': ''
+    },
+    'style': {
+      'flowType': {
+        'name': 'any'
+      },
+      'required': false,
+      'description': ''
+    }
+  }
+};
+
+if (typeof STORYBOOK_REACT_CLASSES !== 'undefined') {
+  STORYBOOK_REACT_CLASSES['test/fixtures/flow1/actual.js'] = {
+    name: 'Button',
+    docgenInfo: Button.__docgenInfo,
+    path: 'test/fixtures/flow1/actual.js'
+  };
+}
---
+++ test/fixtures/flow2/actual.js
@@ -0,0 +1,19 @@
+// @flow
+import React from 'react';
+
+type Props = {|
+  children: string,
+  onClick?: () => any,
+  style?: any,
+|};
+
+const Button = ({ children, onClick, style }: Props) => (
+  <button
+    style={{ }}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
+
+export default Button;
